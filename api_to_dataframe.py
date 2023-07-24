0adbd052d648b2221e0202e13e933ae3efe80ae1
Smarth Arora <129375913+CTM-Giorno@users.noreply.github.com>
2023-07-24 22:26:02 +0530

    Update api_to_dataframe.py

--- api_to_dataframe.py
+++ api_to_dataframe.py
@@ -4,21 +4,26 @@ import pandas as pd
 import time
 
 
-def get_weather(api_key='65326f47edebc6d805c9ecf9238d1f7f',city_name='Delhi'): 
-
+def get_weather(api_key,city_name): 
   url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
   response = requests.get(url)  
-  data = response.json()
-  return data
+  raw_data = response.json()
+  return raw_data
 
-def handle_temp(raw_data,date_list,temp_list):
-
+def handle_temp(raw_data,date_list,data_list):
   temperature = raw_data['main']['temp']
   current_date = date.today().strftime('%Y-%m-%d')
   date_list.append(current_date)
-  temp_list.append(temperature)  
+  data_list.append(temperature)  
+  return current_date,date_list, data_list
 
-  return current_date, temperature , date_list, temp_list
+def data_type(date_list,data_list):
+    while True:
+        data_type= input("Enter the Type of Forcast Data (temprature): ")
+        if data_type=='temprature' :
+            return handle_temp(raw_data,date_list,data_list)
+        else: 
+          print('Data not Available, Enter the Data correctly.')
 
 def gen_df(date_list,temp_list):
   info={
@@ -28,16 +33,30 @@ def gen_df(date_list,temp_list):
   dataframe = pd.DataFrame(info)
   return dataframe
 
+
+
 if __name__ == "__main__":
-  date_list = []
-  temp_list = []
 
   while True:
-    raw_data=get_weather()
-    current_date, temperature,date_list,temp_list = handle_temp(raw_data,date_list,temp_list)
-    dataframe=gen_df(date_list,temp_list)
-    dataframe.to_csv(f'/home/belladonna/Devlopment/Project-Weather_Forecast_Visualization/weather_info({current_date}).csv', index=False)
-    
+    date_list=[]
+    data_list=[]
+    current_date=''
+    # Taking Input of the API and the City.
+    api_key, city_name = input("Enter the API key and the name of the city (seperated by a space): ").split()
+
+    # Getting Raw Data of the API Response.
+    raw_data=get_weather(api_key,city_name)
+
+    # Filtering the Raw Data into list of Date and Data.
+    current_date,date_list, data_list= data_type(date_list,data_list)
+
+    # Generating a DataFrame.
+    dataframe=gen_df(date_list,data_list)
+
+    # Generating a CSV File
+    dataframe.to_csv(f'weather_info{current_date}.csv', index=False)
+
+    print(f"The data is in the created file: 'weather_info{current_date}.csv'")
     break #time.sleep(24 * 60 * 60)
 
     
