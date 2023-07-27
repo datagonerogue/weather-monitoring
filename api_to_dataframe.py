c705ff780d6ff49e935ca59e464abba9ac2ffb56
Smarth Arora <129375913+CTM-Giorno@users.noreply.github.com>
2023-07-27 12:50:40 +0530

    Added functionality for more weather data

--- api_to_dataframe.py
+++ api_to_dataframe.py
@@ -5,30 +5,33 @@ import time
 
 
 def get_weather(api_key,city_name): 
-  url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
+  url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&units=metric&appid={api_key}'
   response = requests.get(url)  
   raw_data = response.json()
   return raw_data
 
-def handle_temp(raw_data,date_list,data_list):
+def handle_weather(raw_data, temp_list, weather_list, humidity_list, wind_speed_list):
   temperature = raw_data['main']['temp']
+  weather_desc = raw_data['weather'][0]['description']
+  humidity = raw_data['main']['humidity']
+  wind_speed = raw_data['wind']['speed']
+
+  temp_list.append(temperature)
+  weather_list.append(weather_desc)
+  humidity_list.append(humidity)
+  wind_speed_list.append(wind_speed)
+  
   current_date = date.today().strftime('%Y-%m-%d')
-  date_list.append(current_date)
-  data_list.append(temperature)  
-  return current_date,date_list, data_list
 
-def data_type(date_list,data_list):
-    while True:
-        data_type= input("Enter the Type of Forcast Data (temprature): ")
-        if data_type=='temprature' :
-            return handle_temp(raw_data,date_list,data_list)
-        else: 
-          print('Data not Available, Enter the Data correctly.')
+  return current_date, temp_list, weather_list, humidity_list, wind_speed_list
 
-def gen_df(date_list,temp_list):
-  info={
-    'Time' :date_list,
-    'Temperature':temp_list
+def gen_df(current_date,temp_list, weather_list, humidity_list, wind_speed_list):
+  info = {
+    'Time': current_date,
+    'Temperature': temp_list,
+    'Weather': weather_list,
+    'Humidity': humidity_list,
+    'Wind Speed': wind_speed_list
   }
   dataframe = pd.DataFrame(info)
   return dataframe
@@ -38,25 +41,36 @@ def gen_df(date_list,temp_list):
 if __name__ == "__main__":
 
   while True:
-    date_list=[]
-    data_list=[]
-    current_date=''
+    temp_list = []
+    weather_list = []
+    humidity_list = []
+    wind_speed_list = []
+    current_date = ''
+
     # Taking Input of the API and the City.
+    days = int(input("Enter the number of days you want to collect daily data for: "))
     api_key, city_name = input("Enter the API key and the name of the city (seperated by a space): ").split()
+    print("Getting the Initial Response...")
 
-    # Getting Raw Data of the API Response.
-    raw_data=get_weather(api_key,city_name)
+    for _ in range(days):
+        # Getting Raw Data of the API Response.
+        raw_data=get_weather(api_key,city_name)
 
-    # Filtering the Raw Data into list of Date and Data.
-    current_date,date_list, data_list= data_type(date_list,data_list)
+        # Filtering the Raw Data into list of Date and Data.
+        current_date, temp_list, weather_list, humidity_list, wind_speed_list= handle_weather(raw_data, temp_list, weather_list, humidity_list, wind_speed_list)
 
-    # Generating a DataFrame.
-    dataframe=gen_df(date_list,data_list)
+        # Generating a DataFrame.
+        dataframe = gen_df(current_date,temp_list, weather_list, humidity_list, wind_speed_list)
+        desired_columns = ['Time', 'Temperature', 'Weather', 'Humidity', 'Wind Speed']
 
-    # Generating a CSV File
-    dataframe.to_csv(f'weather_info{current_date}.csv', index=False)
+        # Generating a CSV File
+        dataframe.to_csv(f'weather_info{current_date}.csv', index=False)
+        print(f'''
+        --------------------------------------------------------
+        {dataframe.style.set_properties(**{'text-align': 'left'})
+}
+        --------------------------------------------------------
+        ''')
 
-    print(f"The data is in the created file: 'weather_info{current_date}.csv'")
-    break #time.sleep(24 * 60 * 60)
-
-    
+        dataframe.to_csv(f'weather_info_{city_name}_{current_date}.csv', index=False)
+        time.sleep(24 * 60 * 60)    
