e6adf863e7ddca4c46a04b50c67b12974d96f9a5
Smarth Arora <129375913+CTM-Giorno@users.noreply.github.com>
2023-07-28 15:07:20 +0530

    redacted the html support

--- api_to_dataframe.py
+++ api_to_dataframe.py
@@ -3,7 +3,7 @@ import requests
 from datetime import date
 import pandas as pd
 import time
-from jinja2 import Environment, FileSystemLoader
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Weather Forecast Visualization Script")
@@ -18,44 +18,32 @@ def get_weather(api_key,city_name):
   raw_data = response.json()
   return raw_data
 
-def handle_weather(raw_data, temp_list, weather_list, humidity_list, wind_speed_list):
-  temperature = raw_data['main']['temp']
-  weather_desc = raw_data['weather'][0]['description']
+def handle_weather(raw_data):
+  temp = raw_data['main']['temp']
+  weather = raw_data['weather'][0]['description']
   humidity = raw_data['main']['humidity']
   wind_speed = raw_data['wind']['speed']
 
-  temp_list.append(temperature)
-  weather_list.append(weather_desc)
-  humidity_list.append(humidity)
-  wind_speed_list.append(wind_speed)
-  
   current_date = date.today().strftime('%Y-%m-%d')
 
-  return current_date, temp_list, weather_list, humidity_list, wind_speed_list
+  return current_date, temp, weather, humidity, wind_speed
 
-def gen_df(current_date,temp_list, weather_list, humidity_list, wind_speed_list):
+def gen_df(current_date,temp, weather, humidity, wind_speed):
   info = {
-    'Time': current_date,
-    'Temperature': temp_list,
-    'Weather': weather_list,
-    'Humidity': humidity_list,
-    'Wind Speed': wind_speed_list
+    'Time': [current_date],
+    'Temperature': [temp],
+    'Weather': [weather],
+    'Humidity': [humidity],
+    'Wind Speed': [wind_speed]
   }
   dataframe = pd.DataFrame(info)
   return dataframe
 
-temp_list = []
-weather_list = []
-humidity_list = []
-wind_speed_list = []
-current_date = ''
 args = parse_arguments()
 api_key = args.api_key
 city_name = args.city_name
 days = args.days
-# Create the Jinja2 environment
-env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template('dataframe_template.html')
+
 for day in range(days):
     if day == 0:
         print("Initiating API Request")
@@ -63,21 +51,10 @@ for day in range(days):
     # Getting Raw Data of the API Response.
     raw_data=get_weather(api_key,city_name)
     # Filtering the Raw Data into list of Date and Data.
-    current_date, temp_list, weather_list, humidity_list, wind_speed_list= handle_weather(raw_data, temp_list, weather_list, humidity_list, wind_speed_list)
+    current_date, temp, weather, humidity, wind_speed= handle_weather(raw_data)
+
     # Generating a DataFrame.
-    dataframe = gen_df(current_date,temp_list, weather_list, humidity_list, wind_speed_list)
-    # Reindex the DataFrame with the desired columns order
-    desired_columns = ['Time', 'Temperature', 'Weather', 'Humidity', 'Wind Speed']
-    dataframe = dataframe.reindex(columns=desired_columns)
-    # Format the DataFrame to HTML using Jinja2
-    dataframe_html = dataframe.to_html(classes='data', escape=False, index=False)
-    # Render the template with the DataFrame HTML
-    output_html = template.render(dataframe=dataframe_html)
-    # Save the styled DataFrame to an HTML file
-    output_file_path = f'weather_info_{city_name}.html'
-    with open(output_file_path, 'a') as f:
-        f.write(output_html)
-    print(f"The data is saved in the file: {output_file_path}")
+    dataframe = gen_df(current_date,temp, weather, humidity, wind_speed)
     # Save the styled DataFrame to an CSV file
     dataframe.to_csv(f'weather_info_{city_name}.csv', index=False, mode='a', header=not day)
     print(f"The data is also saved in the CSV file: 'weather_info_{city_name}_{current_date}.csv'")
